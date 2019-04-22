28d9e511f04d571437672d64bac5d486f301f241
swppppp <sw931110@naver.com>
2019-04-22 23:15:16 +0900

    back버튼 이벤트처리(Dialog처리)

--- app/src/main/java/com/example/ex/Mission2Activity.java
+++ app/src/main/java/com/example/ex/Mission2Activity.java
@@ -1,5 +1,7 @@
 package com.example.ex;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +12,21 @@ public class Mission2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission2);
     }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("작성중인 내용을 저장하지 않고 나가시겠습니까?");
+        builder.setPositiveButton("확인", dialogListener);
+        builder.setNegativeButton("취소", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            finish();
+        }
+    };
 }
