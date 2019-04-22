b5526afac6d78ae6774b71741e90550315d8156b
swppppp <sw931110@naver.com>
2019-04-22 17:14:33 +0900

    button event

--- app/src/main/java/com/example/ex/MainActivity.java
+++ app/src/main/java/com/example/ex/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.ex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.messenger_btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast toast = Toast.makeText(this, "Ok Button Clicked~", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
