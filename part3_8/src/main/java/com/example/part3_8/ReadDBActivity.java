77f2e9edd7b95ead7960652f26d982c017051b8b
swppppp <sw931110@naver.com>
2019-04-23 10:27:46 +0900

    왜안돼지

--- part3_8/src/main/java/com/example/part3_8/ReadDBActivity.java
+++ part3_8/src/main/java/com/example/part3_8/ReadDBActivity.java
@@ -1,13 +1,30 @@
 package com.example.part3_8;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+// 테이블에 저장한 데이터를 뿌리는 화면
 public class ReadDBActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_db);
+
+        TextView titleView=(TextView)findViewById(R.id.read_title);
+        TextView contentView=(TextView)findViewById(R.id.read_content);
+
+        DBHelper helper=new DBHelper(this);
+        SQLiteDatabase db=helper.getWritableDatabase();
+        Cursor cursor=db.rawQuery("select title, content from tb_memo order by _id desc limit 1", null);
+
+        while (cursor.moveToNext()){
+            titleView.setText(cursor.getString(0));
+            contentView.setText(cursor.getString(1));
+        }
+        db.close();
     }
 }
